e4551df62ac8a73ccb4421c5d0febaf71769d7cc
Paul Mudreac <pg.mudreac@hotmail.com>
2018-03-21 06:37:02 +0000

    Minor Changes

--- views/home.js
+++ views/home.js
@@ -1,10 +1,11 @@
-/*global $*/
+/*global $, MovieListView, deleteMovie, getMoviesList, postMovie*/
 function displayAllMovies(x){
     let createContainer = $('#createContainer');
-    let list = $('#movieList');
-    for (let i=0; i<x.results.length; i++){
-        let movie = new MovieListView(x.results[i]);
-        list.append('<li data-idcode="' + movie.id + '">' +
+    let listElement = $('#movieList');
+    let results = x.results;
+    for (let i=0; i<results.length; i++){
+        let movie = new MovieListView(results[i]);
+        listElement.append('<li data-idcode="' + movie.id + '">' +
                         '<img src=' + movie.imageUrl + ' alt="' + movie.title + '"></img></br>' +
                         '<a target="_blank" href="https://ancient-caverns-16784.herokuapp.com/movies/' + movie.id + '">' +movie.title + '</a>' +
                         // '<div>' + movie.description + '</div>' +
@@ -14,7 +15,7 @@ function displayAllMovies(x){
 
     $('.del').on('click', function(){
         deleteMovie($(this.closest('li')).data('idcode'));
-        list.html('');
+        listElement.html('');
         getMoviesList();
     });
 
@@ -25,15 +26,15 @@ function displayAllMovies(x){
     $('#cancel').on('click', function(){
         createContainer.css('display', 'none');
         deleteFormContents();
-    })
+    });
     
     $('#approve').on('click', function(){
         var formInputs = $('#createContainer').children('input, textarea');
         postMovie(formInputs).then(function(){
-            list.html('');
+            listElement.html('');
             getMoviesList();
         });
-    })
+    });
 }
 
 function deleteFormContents() {
@@ -41,5 +42,5 @@ function deleteFormContents() {
         .children('input, textarea')
         .each(function(){
             this.value = '';
-        })
+        });
 }
