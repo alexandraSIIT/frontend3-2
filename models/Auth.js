d607c764815a4416d1e38b68f10b74a7540d1d9c
Andreea Cristea <cristea.andreea18@yahoo.com>
2018-03-27 15:52:00 +0000

    Register form/Token saved in cookie/ Token taken from cookie/ Log out

---
+++ models/Auth.js
@@ -0,0 +1,30 @@
+// This function sends the username and passsword to the API which generates a specific token
+function Registering(baseURL,userName,password){
+    return $.ajax({
+        url: baseURL + "auth/register",
+        method:'POST',
+        dataType:"json",
+        data: { username: userName.val(),
+        	password: password.val()
+        },
+            success:function(response){ 
+                console.log(response);
+            }
+   
+    });
+}
+
+// This function is logging out the user by sending the session's token associated with the user.
+function logOutRequest(baseURL, authToken){
+    const headerName = "X-Auth-Token";
+    $.ajax({
+            url: baseURL + "auth/logout",
+            method:'GET',
+            beforeSend:function(xhr){
+            xhr.setRequestHeader(headerName, authToken);
+            },
+            success:function(response){ 
+                console.log(response.message);
+            }
+    });
+}
--- views/home.js
+++ views/home.js
@@ -1,5 +1,68 @@
 /*global $, MovieListView, deleteMovie, getMoviesList, postMovie*/
 
+$(document).ready(function() {
+    const baseURL = "https://ancient-caverns-16784.herokuapp.com/";
+
+    const registerBtn = $('#register');
+    const registerForm = $('#registerForm');
+    registerBtn.click(function registerFormAppear(){
+       registerForm.addClass('show').removeClass('hide');
+    });
+    
+    const userName = $("[name=Username]");
+    const password = $("[name=Password]");
+    const registerSubmitBtn = $('#register-submit');
+    
+    registerSubmitBtn.click(hideRegisterForm);
+    registerSubmitBtn.click(registerSubmitClick);
+    
+    const logOutBtn = $('#log-out');
+    logOutBtn.click(onClickLogOut);
+// This function recalls the getCookiesAsObject for the const authToken to have the 
+// current token value saved in the cookies.
+// Also calls logOutRequest function
+function onClickLogOut(){
+    logOutBtn.addClass('hide').removeClass('show');
+    const authToken= getCookiesAsObject();
+    logOutRequest(baseURL,authToken);
+}
+
+// This function is called when clicking the submit button
+function registerSubmitClick(){
+    Registering(baseURL, userName, password).then(setCookie);
+}
+
+// This function prevents the page from refreshing after submit button click.
+// Also it hides the register form.
+function hideRegisterForm(event){
+    event.preventDefault();
+    registerForm.addClass('hide').removeClass('show');
+    logOutBtn.addClass('show').removeClass('hide');
+    
+}
+});
+
+// This function sets the token's value as a cookie "token=tokenValue"
+function setCookie(response){
+    const accessToken = response.accessToken;
+    document.cookie = "token=" + accessToken;
+}
+
+// This function takes the token that was previously saved in cookies
+function getCookiesAsObject() {
+    const cookiesString = document.cookie;
+    const cookiesArray = cookiesString.split("; ");
+    const cookies = {};
+    cookiesArray.forEach(function(c) {
+        const cookie = c.split("=");
+        const key = cookie[0]; 
+        const value = cookie[1];
+        cookies[key] = value;
+    });
+    const authToken = cookies.token;
+    return authToken;
+} 
+
 //Function below renders the movie list "list" in a user friendly format
 //Then it attaches some event listeners for interface buttons
 function displayAllMovies(list){
